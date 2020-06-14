14b1526a12fc2e2bd73a934fb9577a344c85658d
Naseem Saadeddine <49487193+nbass668@users.noreply.github.com>
2020-06-14 16:07:24 +0300

    Update Elementor-Remove-Bloat.php

--- Elementor-Remove-Bloat.php
+++ Elementor-Remove-Bloat.php
@@ -31,7 +31,7 @@ function dequeue_elementor_fontawesome() {
 	}
 }
 
-//Remove Gutenberg Block Library CSS from loading on the frontend
+// Remove Gutenberg Block Library CSS from loading on the frontend
 function remove_block_css() {
 wp_dequeue_style( 'wp-block-library' ); // WordPress core
 wp_dequeue_style( 'wp-block-library-theme' ); // WordPress core
@@ -39,6 +39,37 @@ wp_dequeue_style( 'wc-block-style' ); // WooCommerce
 wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront theme
 }
 
+
+// Disable the emoji's
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	
+	// Remove from TinyMCE
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+
+// Filter out the tinymce emoji plugin.
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+
+// Disable WP-EMBED
+function my_deregister_scripts(){
+  wp_deregister_script( 'wp-embed' );
+}
+
+add_action( 'wp_footer', 'my_deregister_scripts' ); // Disable WP-EMBED
+add_action( 'init', 'disable_emojis' ); // Disable the emoji's
 add_filter('use_block_editor_for_post_type', '__return_false', 10); // Disable Gutenberg Block Library
 add_action( 'elementor/frontend/after_register_styles', 'dequeue_elementor_fontawesome', 20 ); // Disable fontawesome
 add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 ); // Disable Gutenberg Block Library
